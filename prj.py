935c46345cfeeb018a8dd4c7f9cb74ff9fa07cc1
Rajveer singh dhaliwal <104855149+rajveersd13@users.noreply.github.com>
2022-11-30 15:12:25 +0530

    Delete prj.py

--- prj.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title(' TDS GA 8')
-st.subheader('odd even checking  App')
-placeholder_Number = st.empty()
-n = placeholder_Number.number_input('Enter Number')
-if (n % 2) == 0:
-   st.write("your number is even")
-else:
-   st.write("your number is odd")
